516991eef636e33851c653872281ef44cd2750a5
sik0527 <sik0527@my.londonmet.ac.uk>
2026-04-16 16:21:56 +0100

    Add files via upload

---
+++ Gadget.java
@@ -0,0 +1,27 @@
+public class Gadget {
+    private String model;
+    private double price;
+    private int weight;
+    private String size;
+
+    public Gadget(String model, double price, int weight, String size) {
+        this.model = model;
+        this.price = price;
+        this.weight = weight;
+        this.size = size;
+    }
+
+    // Accessor methods
+    public String getModel() { return model; }
+    public double getPrice() { return price; }
+    public int getWeight() { return weight; }
+    public String getSize() { return size; }
+
+    // Display method
+    public void display() {
+        System.out.println("Model: " + model);
+        System.out.println("Price: £" + price);
+        System.out.println("Weight: " + weight + " g");
+        System.out.println("Size: " + size);
+    }
+}
